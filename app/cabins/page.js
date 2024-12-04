2dc80869e196ba23b255f6a75f0f033910a96776
J.S. Desmarais <js1.desmarais@gmail.com>
2024-12-03 22:07:33 -0500

    started implementing data-driven renderings by experimenting with client-server boundary

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -12,7 +12,7 @@ export default function Page() {
 	return (
 		<div>
 			<h1 className='text-4xl mb-5 text-accent-400 font-medium'>Our Luxury Cabins</h1>
-			<p className='text-primary-200 text-lg mb-10'>
+			<p className='text-primary-200 text-lg mb-10 '>
 				Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites.
 				Imagine waking up to beautiful mountain views, spending your days exploring the
 				dark forests around, or just relaxing in your private hot tub under the stars.
