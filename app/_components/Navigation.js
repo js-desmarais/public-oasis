b750c8b08826d92b4db8e4b2dc95bcde24e190b9
J.S. Desmarais <js1.desmarais@gmail.com>
2024-12-17 17:23:32 -0500

    finished conditional rendering of components meant for auth users only

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Navigation() {
+import { auth } from '../_lib/auth';
+
+export default async function Navigation() {
+	const session = await auth();
+	console.log(session);
+
 	return (
 		<nav className='z-10 text-xl'>
 			<ul className='flex gap-16 items-center'>
@@ -15,8 +21,24 @@ export default function Navigation() {
 					</Link>
 				</li>
 				<li>
-					<Link href='/account' className='hover:text-accent-400 transition-colors'>
-						Guest area
+					<Link
+						href='/account'
+						className='hover:text-accent-400 transition-colors flex gap-4 items-center'
+					>
+						{session?.user?.image ? (
+							<div className='relative h-8 w-8'>
+								<Image
+									fill
+									className='rounded-full'
+									src={session.user.image}
+									alt={session.user.name}
+									referrerPolicy='no-referrer'
+								/>
+							</div>
+						) : (
+							''
+						)}
+						<span>Guest area</span>
 					</Link>
 				</li>
 			</ul>
--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,5 +1,7 @@
+import { auth } from '../_lib/auth';
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
 import DateSelector from './DateSelector';
+import LoginMessage from './LoginMessage';
 import ReservationForm from './ReservationForm';
 
 export default async function Reservation({ cabin }) {
@@ -7,11 +9,16 @@ export default async function Reservation({ cabin }) {
 		getSettings(),
 		getBookedDatesByCabinId(cabin.id),
 	]);
+	const session = await auth();
 
 	return (
 		<div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
 			<DateSelector cabin={cabin} settings={settings} bookedDates={bookedDates} />
-			<ReservationForm cabin={cabin} />
+			{session?.user ? (
+				<ReservationForm cabin={cabin} user={session.user} />
+			) : (
+				<LoginMessage />
+			)}
 		</div>
 	);
 }
