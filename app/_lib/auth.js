cbbd4e74e3d45578af8b540a200595771b17df6c
J.S. Desmarais <js1.desmarais@gmail.com>
2024-12-18 16:47:36 -0500

    updated auth to save the guest information and update the session with guestId

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 const authConfig = {
 	providers: [
@@ -11,6 +12,23 @@ const authConfig = {
 	callbacks: {
 		authorized({ auth, req }) {
 			return !!auth?.user;
+		},
+		async signIn({ user, account, profile }) {
+			try {
+				const existingGuest = await getGuest(user.email);
+
+				if (!existingGuest) await createGuest({ email: user.email, fullName: user.name }); // NOTE Very important to await, otherwise moving on to the session callback with a guest that hasn't been created yet.
+
+				return true;
+			} catch {
+				return false;
+			}
+		},
+		async session({ session, user }) {
+			const guest = await getGuest(session.user.email);
+			session.user.guestId = guest.id;
+
+			return session;
 		},
 	},
 	pages: {
